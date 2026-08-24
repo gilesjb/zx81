684afeb56e7c7128d74a513ab426d9c5f4408411
gilesjb <gilesjb@gmail.com>
2026-08-24 14:53:09 -0700

    driver: Switched to using X macros for track

--- driver.c
+++ driver.c
@@ -8,82 +8,55 @@ zcc +zx81 -create-app -o build/driver.bin "$0"; exit
 #include <input.h>
 #include <intrinsic.h>
 
-#define W 0x00
-#define WW W,W
-#define WWW W,W,W
-#define L 0x76
-#define B 0x80
-#define TOP L,L,L,L,L,L,L,L,L,L,L,L,L,L,L
+#define _ X(0x00) // space
+#define L X(0x76) // newline
+#define B X(0x80) // black square
+#define G X(0x08) // grey square
+#define U X(0x0a) // upper half grey
+#define D X(0x09) // bottom half grey
+#define A X(0x26) // 'A'
 
-#define G 0x08
-#define U 0x0a
-#define D 0x09
+#define SCREEN1 \
+    L _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ D D \
+    L _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ U U \
+    L _ _ _ _ _ _ _ _ _ _ _ _ G _ _ _ _ _ _ G \
+    L _ _ _ _ _ _ _ _ _ D D _ _ _ _ _ _ _ _ _ _ D D \
+    L _ _ _ _ _ _ _ _ _ U U _ _  _ _ _ _ _ _ _ _ U U \
+    L _ _ _ _ _ _ G G _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ G G \
+    L _ _ _ D D D _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ D D D \
+    L _ _ _ U U U _ _ _ _ _ _ _ _ D A D _ _ _ _ _ _ _ _ _ U U U \
+    L G G G _ _ _ _ _ _ _ _ _ _ _ B U B _ _ _ _ _ _ _ _ _ _ _ _ G G G
 
-#define DAD D,0x26,D // top of car
-#define BUB B,U,B    // bottom of car
+#define SCREEN2 \
+    L \
+    L _ _ _ _ _ _ _ _ _ _ _ _ _ _ G _ _ G \
+    L _ _ _ _ _ _ _ _ _ _ _ D _ _ _ _ _ _ _ _ D \
+    L _ _ _ _ _ _ _ _ _ _ _ U _ _ _ _ _ _ _ _ U \
+    L _ _ _ _ _ _ _ _ G G _ _ _ _ _ _ _ _ _ _ _ _ G G \
+    L _ _ _ _ _ D D _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ D D \
+    L _ _ _ _ _ U U _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ U U \
+    L _ _ G G G _ _ _ _ _ _ _ _ _ D A D _ _ _ _ _ _ _ _ _ _ G G G \
+    L D D _ _ _ _ _ _ _ _ _ _ _ _ B U B _ _ _ _ _ _ _ _ _ _ _ _ _ D D
 
-unsigned char buffer1[] = {
-    TOP,
+#define SCREEN3 \
+    L \
+    L _ _ _ _ _ _ _ _ _ _ _ _ _ D _ _ _ _ D \
+    L _ _ _ _ _ _ _ _ _ _ _ _ _ U _ _ _ _ U \
+    L _ _ _ _ _ _ _ _ _ _ G G _ _ _ _ _ _ _ _ G G \
+    L _ _ _ _ _ _ _ D D _ _ _ _ _ _ _ _ _ _ _ _ _ _ D D \
+    L _ _ _ _ _ _ _ U U _ _ _ _ _ _ _ _ _ _ _ _ _ _ U U \
+    L _ _ _ _ G G G _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ G G G \
+    L _ D D D _ _ _ _ _ _ _ _ _ _ D A D _ _ _ _ _ _ _ _ _ _ _ D D D \
+    L _ U U U _ _ _ _ _ _ _ _ _ _ B U B _ _ _ _ _ _ _ _ _ _ _ U U U  
 
-    // prev line should be just long enough we can 'borrow' up to 15 spaces from it 
-    WWW, WWW, WWW, WWW, WWW, D,D,L,  //15
-    WWW, WWW, WWW, WWW, WWW, U,U,L,  //15
+#define TOP L L L L L L L L L L L L L L
+#define END L L L L L L L
 
-    WWW, WWW, WWW, WWW, G, WWW, WWW, G,L, //12
-
-    WWW, WWW, WWW, D,D, WW, WWW, WWW, WW, D,D,L, //9
-    WWW, WWW, WWW, U,U, WW, WWW, WWW, WW, U,U,L, //9
-
-    WWW, WWW, G,G, WW, WWW, WWW, WWW, WWW, WW, G,G,L, //6
-
-    WWW, D,D,D, WWW, WWW, WWW, WWW, WWW, WWW, WW, D,D,D,L, //3
-    WWW, U,U,U, WWW, WWW, WWW, DAD, WWW, WWW, WW, U,U,U,L, //3
-    
-    G,G,G, WWW,WWW,WWW,WWW,BUB,WWW,WWW,WWW, WW, G,G,G,L, //0
-
-    L,L,L,L,L,L
-};
-
-unsigned char buffer2[] = {
-    TOP,
-
-    L,
-    WW, WWW, WWW, WWW, WWW, G, WW, G,L, //14
-
-    WW, WWW, WWW, WWW, D, WWW, WWW, WW, D,L, //11
-    WW, WWW, WWW, WWW, U, WWW, WWW, WW, U,L, //11
-    
-    WW, WWW, WWW, G,G, WWW,WWW,WWW,WWW, G,G,L, //8
-
-    WW, WWW, D,D, WWW, WWW, WWW, WWW, WWW, WWW, D,D,L, //5
-    WW, WWW, U,U, WWW, WWW, WWW, WWW, WWW, WWW, U,U,L, //5
-
-    WW, G,G,G, W, WWW, WWW, WWW, DAD, WWW, WWW, WWW, G,G,G,L, //2
-
-    D,D, W, WWW, WWW, WWW, WWW, BUB, WWW, WWW, WWW, WWW, D,D,L, //-1
-
-    L,L,L,L,L,L
-};
-
-unsigned char buffer3[] = {
-    TOP,
-
-    L,
-    WWW, WWW, WWW, WWW, W, D, WW, WW, D,L, //13
-    WWW, WWW, WWW, WWW, W, U, WW, WW, U,L, //13
-
-    WWW, WWW, WWW, W, G,G, WWW, WWW, WW, G,G,L, //10
-
-    WWW, WWW, W, D,D, WWW, WWW, WWW, WWW, WW, D,D,L, //7
-    WWW, WWW, W, U,U, WWW, WWW, WWW, WWW, WW, U,U,L, //7
-
-    WWW, W, G,G,G, WWW,WWW,WWW,WWW,WWW,WWW, G,G,G,L, //4
-
-    W, D,D,D, WWW,WWW,WWW,WW,DAD,W,WWW,WWW,WWW,D,D,D,L, //1
-    W, U,U,U, WWW,WWW,WWW,WW,BUB,W,WWW,WWW,WWW,U,U,U,L, //1
-
-    L,L,L,L,L,L
-};
+#define X(c) c,
+unsigned char buffer1[] = { TOP SCREEN1 END 0x00 };
+unsigned char buffer2[] = { TOP SCREEN2 END 0x00 };
+unsigned char buffer3[] = { TOP SCREEN3 END 0x00 };
+#undef X
 
 unsigned char *buffers[] = {buffer1, buffer2, buffer3};
 
